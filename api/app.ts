c3d51c0f560ca4930d21f67dddd441ca485beae3
axdevil <deb.devel@outlook.es>
2024-02-12 08:48:19 -0600

    Server configuration

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,17 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.listen(process.env.PORT, () => {
-    console.log(`Listen on ${process.env.PORT}`)
-})
+    console.log(`Listen on ${process.env.PORT}`);
+});
