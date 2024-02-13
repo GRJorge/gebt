96e89f850fa0838320b7155cf61d11ab34292463
axdevil <deb.devel@outlook.es>
2024-02-12 19:50:54 -0600

    Save user in database

--- api/controller/user.ts
+++ api/controller/user.ts
@@ -1,7 +1,24 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import User from '../model/user';
 
 export default {
-    newUser: function (req: Request, res: Response) {
-        res.send('User new')
+    newUser: async function (req: Request, res: Response) {
+        const { name, lastname, email, password } = req.body;
+
+        try {
+            const newUser = await new User({ name, lastname, email, password }).save();
+
+            res.status(200).json({ msg: 'User created', data: newUser });
+        } catch (error: any) {
+            if (error.code === 11000 && error.keyPattern.email) {
+                res.status(400).json({ msg: 'Email in use', valueWithError: 'email', valueError: error.keyValue.email, error: error });
+            } else if (error.name === 'ValidationError') {
+                res.status(400).json({ msg: error.message, error });
+            } else {
+                res.status(500).json({ msg: 'Internal Error', error });
+            }
+
+            console.log(error);
+        }
     },
 };
