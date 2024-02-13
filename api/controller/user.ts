99398c48ebef09d043a52acfa222400a60db015b
axdevil <deb.devel@outlook.es>
2024-02-12 20:07:45 -0600

    Password encryption

--- api/controller/user.ts
+++ api/controller/user.ts
@@ -1,24 +1,31 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../model/user';
 
 export default {
-    newUser: async function (req: Request, res: Response) {
+    newUser: function (req: Request, res: Response) {
         const { name, lastname, email, password } = req.body;
 
-        try {
-            const newUser = await new User({ name, lastname, email, password }).save();
+        bcrypt.hash(password, 10, async (err, hashPassword) => {
+            if (!err) {
+                try {
+                    const newUser = await new User({ name, lastname, email, password: hashPassword }).save();
 
-            res.status(200).json({ msg: 'User created', data: newUser });
-        } catch (error: any) {
-            if (error.code === 11000 && error.keyPattern.email) {
-                res.status(400).json({ msg: 'Email in use', valueWithError: 'email', valueError: error.keyValue.email, error: error });
-            } else if (error.name === 'ValidationError') {
-                res.status(400).json({ msg: error.message, error });
+                    res.status(200).json({ msg: 'User created', data: newUser });
+                } catch (error: any) {
+                    if (error.code === 11000 && error.keyPattern.email) {
+                        res.status(400).json({ msg: 'Email in use', valueWithError: 'email', valueError: error.keyValue.email, error: error });
+                    } else if (error.name === 'ValidationError') {
+                        res.status(400).json({ msg: error.message, error });
+                    } else {
+                        res.status(500).json({ msg: 'Internal Error', error });
+                    }
+
+                    console.log(error);
+                }
             } else {
-                res.status(500).json({ msg: 'Internal Error', error });
+                res.status(500).json({ msg: 'Internal Error', err });
             }
-
-            console.log(error);
-        }
+        });
     },
 };
