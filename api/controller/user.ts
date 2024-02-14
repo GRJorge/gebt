449cd929a34ce54e167a3305032fde9351f18e02
axdevil <deb.devel@outlook.es>
2024-02-13 19:31:23 -0600

    SignIn Petition

--- api/controller/user.ts
+++ api/controller/user.ts
@@ -10,6 +10,7 @@ export default {
             if (!err) {
                 try {
                     const newUser = await new User({ name, lastname, email, password: hashPassword }).save();
+                    req.session.user = newUser._id.toString();
 
                     res.status(200).json({ msg: 'User created', data: newUser });
                 } catch (error: any) {
@@ -27,5 +28,31 @@ export default {
                 res.status(500).json({ msg: 'Internal Error', err });
             }
         });
+    },
+    signIn: async function (req: Request, res: Response) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).lean();
+
+        if (user?.email != null || user?.password != null) {
+            if (user) {
+                bcrypt.compare(password, user.password, (error, result) => {
+                    if (!error) {
+                        if (result) {
+                            req.session.user = user._id.toString();
+                            res.status(200).json({ msg: 'Successful Login' });
+                        } else {
+                            res.status(400).json({ msg: 'Incorrect Password' });
+                        }
+                    } else {
+                        res.status(500).json({ msg: 'Internal Error', error });
+                    }
+                });
+            } else {
+                res.status(400).json({ msg: 'Unregistered user', valueError: email });
+            }
+        } else {
+            res.status(500).json({ msg: 'Data required' });
+        }
     },
 };
--- api/routes/user.ts
+++ api/routes/user.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import controller from '../controller/user';
 const router = Router();
 
-router.post('/new', controller.newUser)
+router.post('/new', controller.newUser);
+router.post('/signIn', controller.signIn);
 
 export default router;
