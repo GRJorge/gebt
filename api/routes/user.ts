6e38a33af134a8dc1a6d59f48d0d5f43c0c4ce5d
axdevil <deb.devel@outlook.es>
2024-02-14 19:24:48 -0600

    Sign Out route

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -4,5 +4,6 @@ const router = Router();
 
 router.post('/new', controller.newUser);
 router.post('/signIn', controller.signIn);
+router.post('/signOut', controller.signOut);
 
 export default router;
