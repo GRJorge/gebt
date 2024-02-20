538e6fea017663616194e19084af4b6a2cceabc8
axdevil <deb.devel@outlook.es>
2024-02-19 18:27:52 -0600

    Ruta de sign-up

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'signUp',
+        title: 'Create account',
+        component: SignUpComponent,
+    },
+];
