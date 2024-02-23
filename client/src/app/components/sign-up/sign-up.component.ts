3b2e4084e74b464fa2f46e048a8ab27e1ab993a8
axdevil <deb.devel@outlook.es>
2024-02-22 23:14:06 -0600

    Email in use error

--- client/src/app/components/sign-up/sign-up.component.ts
+++ client/src/app/components/sign-up/sign-up.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { User } from '../../interfaces/user.interface';
 import { NotificationComponent } from '../general/notification/notification.component';
+import { LoadingComponent } from '../general/loading/loading.component';
 
 @Component({
     selector: 'sign-up',
     standalone: true,
-    imports: [ReactiveFormsModule, NotificationComponent],
+    imports: [ReactiveFormsModule, NotificationComponent, LoadingComponent],
     templateUrl: './sign-up.component.html',
     styleUrl: './sign-up.component.scss',
 })
@@ -15,6 +16,7 @@ export class SignUpComponent {
     constructor(private userService: UserService) {}
 
     step = 0;
+    errorDuplicatedEmail = false;
 
     emailControl = new FormControl('', [Validators.required, Validators.minLength(6), Validators.email]);
 
@@ -36,6 +38,7 @@ export class SignUpComponent {
         switch (this.step) {
             case 0:
                 if (this.emailControl.valid) {
+                    this.errorDuplicatedEmail = false
                     this.nextStep();
                 }
                 break;
@@ -48,27 +51,34 @@ export class SignUpComponent {
                 if (this.formName.valid) {
                     this.nextStep();
 
-                    const newUser: User = {
-                        name: this.formName.get('name')!.value!,
-                        lastname: this.formName.get('lastname')!.value!,
-                        email: this.emailControl!.value!,
-                        password: this.formPassword.get('confirm')!.value!,
-                    };
+                    setTimeout(() => {
+                        const newUser: User = {
+                            name: this.formName.get('name')!.value!,
+                            lastname: this.formName.get('lastname')!.value!,
+                            email: this.emailControl!.value!,
+                            password: this.formPassword.get('confirm')!.value!,
+                        };
 
-                    this.userService.newUser(newUser).subscribe({
-                        next: () => {
-                            alert('Usuario guardado');
-                        },
-                        error: () => {
-                            alert('Error');
-                        },
-                    });
+                        this.userService.newUser(newUser).subscribe({
+                            next: () => {
+                                alert('Usuario creado')
+                            },
+                            error: (error) => {
+                                if(error.status === 400){
+                                    if(error.error.valueWithError === 'email'){
+                                        this.errorDuplicatedEmail = true;
+                                        this.step = 0;
+                                    }
+                                }
+                            }
+                        })
+                    }, 1000);
                 }
                 break;
         }
     }
     nextStep() {
-        if (this.step < 2) {
+        if (this.step < 3) {
             this.step += 1;
         }
     }
