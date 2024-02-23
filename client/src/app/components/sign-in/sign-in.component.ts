b25abd6cd7a16227b05e95a5b13e1069786edaab
axdevil <deb.devel@outlook.es>
2024-02-23 00:30:35 -0600

    Validacion de formulario

--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NotificationComponent } from '../general/notification/notification.component';
 
 @Component({
     selector: 'app-sign-in',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, NotificationComponent],
     templateUrl: './sign-in.component.html',
     styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
-    constructor(private router: Router){}
+    constructor(private router: Router) {}
 
-    redirectSignUp(){
-        this.router.navigate(['/signup'])
+    form = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(6)]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+
+    getControlError(control: string, error: string): boolean {
+        return this.form.get(control)?.getError(error);
+    }
+
+    redirectSignUp() {
+        this.router.navigate(['/signup']);
     }
 }
