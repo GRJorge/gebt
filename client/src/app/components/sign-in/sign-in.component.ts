9cd06b710078662f3d6022acfabab373b88f7d6f
axdevil <deb.devel@outlook.es>
2024-02-23 19:21:27 -0600

    Inicio de sesion y manejor de errores 400

--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NotificationComponent } from '../general/notification/notification.component';
+import { UserService } from '../../services/user.service';
+import { UserSignIn } from '../../interfaces/user.interface';
+import { LoadingComponent } from '../general/loading/loading.component';
 
 @Component({
     selector: 'app-sign-in',
     standalone: true,
-    imports: [ReactiveFormsModule, NotificationComponent],
+    imports: [ReactiveFormsModule, NotificationComponent, LoadingComponent],
     templateUrl: './sign-in.component.html',
     styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
-    constructor(private router: Router) {}
+    constructor(private userService: UserService, private router: Router) {}
+
+    valid = false;
+    unregisteredUserError = false;
+    incorrectPasswordError = false;
 
     form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(6)]),
@@ -24,5 +31,37 @@ export class SignInComponent {
 
     redirectSignUp() {
         this.router.navigate(['/signup']);
+    }
+
+    signIn() {
+        if (this.form.valid) {
+            this.valid = true;
+
+            setTimeout(() => {
+                const user: UserSignIn = {
+                    email: this.form.get('email')!.value!,
+                    password: this.form.get('password')!.value!,
+                };
+
+                this.userService.signin(user).subscribe({
+                    next: () => {
+                        this.router.navigate(['/']);
+                    },
+                    error: (error) => {
+                        this.unregisteredUserError = false;
+                        this.incorrectPasswordError = false;
+
+                        if (error.status === 400) {
+                            if (error.error.valueWithError === 'password') {
+                                this.incorrectPasswordError = true;
+                            } else if (error.error.valueWithError === 'email') {
+                                this.unregisteredUserError = true;
+                            }
+                        }
+                        this.valid = false;
+                    },
+                });
+            }, 1000);
+        }
     }
 }
