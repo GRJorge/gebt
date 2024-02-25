70e2009e87a1f916f16b5470528b5023820ae555
axdevil <deb.devel@outlook.es>
2024-02-24 19:10:01 -0600

    Comentarios

--- api/controller/user.ts
+++ api/controller/user.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from 'express';
 import { ResponseInternalError } from '../utils/functions';
 import bcrypt from 'bcrypt';
-import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import User from '../model/user';
 
 export default {
     newUser: function (req: Request, res: Response) {
         const { name, lastname, email, password } = req.body;
-
+        //HASH A LA CONTRASEÑA PROPORCIONADA
         bcrypt.hash(password, 10, async (err, hashPassword) => {
             if (!err) {
                 try {
-                    const newUser = await new User({ name, lastname, email, password: hashPassword }).save();
+                    const newUser = await new User({ name, lastname, email, password: hashPassword }).save(); //GUARDADO EN LA DB
                     //CREACION DE TOKEN
                     jwt.sign({ user: newUser._id }, process.env.SECRET_KEY!, { expiresIn: '30 days' }, (error: any, token: any) => {
                         if (!error) {
-                            res.status(200).json({ msg: 'User created', data: newUser, token });
+                            res.status(200).json({ msg: 'User created', data: newUser, token }); //OK
                         } else {
                             ResponseInternalError(res, error);
                         }
@@ -38,24 +38,26 @@ export default {
     },
     signIn: async function (req: Request, res: Response) {
         const { email, password } = req.body;
-        let missingData: string[] = [];
+        let missingData: string[] = []; //DATOS FALTANTES
 
-        const user = await User.findOne({ email }).lean();
-
+        const user = await User.findOne({ email }).lean(); //BUSQUEDA DE USUARIO EN DB
+        //LLENADO DE DATOS FALTANTES
         if (!email) {
             missingData.push('email');
         }
         if (!password) {
             missingData.push('password');
         }
-
+        //COMPROBAR SI NO FALTA UN DATO EN EL REQ
         if (missingData.length === 0) {
             if (user) {
+                //SI EXISTE UN USUARIO SE COMPARA EL PASSWORD DADO CON EL GUARDADO ENCRIPTADO
                 bcrypt.compare(password, user.password, (error, result) => {
                     if (!error) {
                         if (result) {
                             res.status(200).json({ msg: 'Successful Login' });
                         } else {
+                            //SI LA CONTRASEÑA PROPORCIONADA ES INCORRECTA
                             res.status(400).json({ msg: 'Incorrect Password', valueWithError: 'password' });
                         }
                     } else {
@@ -63,6 +65,7 @@ export default {
                     }
                 });
             } else {
+                //SI NO EXISTE USUARIO EN LA DB
                 res.status(400).json({ msg: 'Unregistered user', valueWithError: 'email', valueError: email });
             }
         } else {
