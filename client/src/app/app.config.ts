c96b14c2eecf4f6be076a3d3fa1cfb9532909d71
axdevil <deb.devel@outlook.es>
2024-02-26 01:16:29 -0600

    Cookie service como proveedor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from './interceptors/error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorHandlerInterceptor]))],
+    providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorHandlerInterceptor])), CookieService],
 };
