bdaf6e15f3b180d1362948c7e158040c0076b80b
axdevil <deb.devel@outlook.es>
2024-02-26 10:26:44 -0600

    Manejo de errores 403

--- client/src/app/interceptors/error-handler.interceptor.ts
+++ client/src/app/interceptors/error-handler.interceptor.ts
@@ -1,11 +1,19 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
+
     return next(req).pipe(
         catchError((error: HttpErrorResponse) => {
-            if(error.status === 0){
-                console.log('Error desconocido')
+            if (error.status === 0) {
+                console.log('Error desconocido');
+            } else if (error.status === 500) {
+                console.log('Internal error');
+            } else if (error.status === 403) {
+                router.navigate(['/signin']);
             }
 
             return throwError(() => error);
