74d30223b0aac4fd234b12df767cfd3370eeae52
axdevil <deb.devel@outlook.es>
2024-02-26 00:28:44 -0600

    Funcion para verificacion de tokens

--- api/utils/functions.ts
+++ api/utils/functions.ts
@@ -1,5 +1,24 @@
-import { Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export function ResponseInternalError(res: Response, error: any) {
     res.status(500).json({ msg: 'Internal Error', error });
+}
+
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (bearerHeader) { //REVISAR SI EXISTE TOKEN
+        const token = bearerHeader.split(' ')[1];
+        //VERIFICAR EL TOKEN
+        jwt.verify(token, process.env.SECRET_KEY!, (error) => {
+            if (error) {
+                res.status(403).json({ msg: 'Invalid Token' });
+            } else {
+                next();
+            }
+        });
+    } else {
+        res.status(403).json({ msg: 'Non-exist Token' });
+    }
 }
