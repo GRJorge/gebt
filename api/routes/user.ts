2175744436efb30c08f1a9bb31365851a656977d
axdevil <deb.devel@outlook.es>
2024-02-26 09:03:36 -0600

    Ruta para verificacion de token

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Router, Response, Request } from 'express';
 import controller from '../controller/user';
+import { verifyToken } from '../utils/token';
 const router = Router();
 
 router.post('/new', controller.newUser);
 router.post('/signIn', controller.signIn);
+router.post('/verifytoken', verifyToken, (req: Request, res: Response) => {
+    res.status(200).json({ msg: 'Ok Token' });
+});
 
 export default router;
