e99347924eba8fbd2959fc045f28df323cf59241
axdevil <deb.devel@outlook.es>
2024-02-26 19:45:30 -0600

    Added TokenInterceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from './interceptors/error-handler.interceptor';
+import { tokenInterceptor } from './interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorHandlerInterceptor])), CookieService],
+    providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorHandlerInterceptor, tokenInterceptor])), CookieService],
 };
