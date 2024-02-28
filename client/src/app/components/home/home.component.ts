e5125e16cc44bb29f3a87450a3b7dd8572d746ba
axdevil <deb.devel@outlook.es>
2024-02-28 09:18:30 -0600

    Top Panel in Home

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component';
+import { TopPanelComponent } from './top-panel/top-panel.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [NavigationComponent],
+    imports: [NavigationComponent, TopPanelComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
