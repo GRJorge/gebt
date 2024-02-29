2cd2475cd38128392ae3b91ffeb113e30e054b6c
axdevil <deb.devel@outlook.es>
2024-02-29 12:45:14 -0600

    Middleware de verificacion de token en peticion

--- api/routes/patient.ts
+++ api/routes/patient.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { verifyToken } from '../utils/token';
 import controller from '../controller/patient';
 const router = Router();
 
-router.post('/new', controller.new)
+router.post('/new', verifyToken, controller.new)
 
 export default router
