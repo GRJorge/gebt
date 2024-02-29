82094b59e7e442abb9273b1013c681f8370fc8dc
axdevil <deb.devel@outlook.es>
2024-02-29 11:05:48 -0600

    Model Schema for Patients

---
+++ api/model/patient.ts
@@ -0,0 +1,12 @@
+import { Schema, model, Types } from 'mongoose';
+
+const schema = new Schema({
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    phone: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    gender: { type: String, required: true },
+    user: { type: Types.ObjectId, ref: 'User', required: true },
+});
+
+export default model('Patient', schema);
