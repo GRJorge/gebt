d7aebe2c10f3d7f8cf19b7b61b54e7a73a254207
axdevil <deb.devel@outlook.es>
2024-02-29 13:32:10 -0600

    Opciones de schema

--- api/model/patient.ts
+++ api/model/patient.ts
@@ -1,12 +1,18 @@
 import { Schema, model, Types } from 'mongoose';
 
-const schema = new Schema({
-    name: { type: String, required: true },
-    lastname: { type: String, required: true },
-    phone: { type: String, required: true },
-    birthday: { type: Date, required: true },
-    gender: { type: String, required: true },
-    user: { type: Types.ObjectId, ref: 'User', required: true },
-});
+const schema = new Schema(
+    {
+        name: { type: String, required: true },
+        lastname: { type: String, required: true },
+        phone: { type: String, required: true },
+        birthday: { type: Date, required: true },
+        gender: { type: String, required: true },
+        user: { type: Types.ObjectId, ref: 'User', required: true },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
 
 export default model('Patient', schema);
