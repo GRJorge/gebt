748b965807fbe418b41a17e16096199aa6f09f86
axdevil <deb.devel@outlook.es>
2024-03-01 19:34:48 -0600

    Basic patient card

--- client/src/app/components/patients/patient-card/patient-card.component.ts
+++ client/src/app/components/patients/patient-card/patient-card.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'patient-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './patient-card.component.html',
-  styleUrl: './patient-card.component.scss'
+    selector: 'patient-card',
+    standalone: true,
+    imports: [],
+    templateUrl: './patient-card.component.html',
+    styleUrl: './patient-card.component.scss',
 })
-export class PatientCardComponent {
-
-}
+export class PatientCardComponent {}
