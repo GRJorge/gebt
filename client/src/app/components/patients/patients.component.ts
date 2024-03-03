967afa7b25dae2a797a612eaa16fccef807fa0fe
axdevil <deb.devel@outlook.es>
2024-03-02 19:45:05 -0600

    Integracion de formulario a seccion pacientes

--- client/src/app/components/patients/patients.component.ts
+++ client/src/app/components/patients/patients.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { PatientCardComponent } from './patient-card/patient-card.component';
+import { NewPatientComponent } from './new-patient/new-patient.component';
 
 @Component({
     selector: 'patients',
     standalone: true,
-    imports: [],
+    imports: [PatientCardComponent, NewPatientComponent],
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss',
 })
-export class PatientsComponent {}
+export class PatientsComponent {
+    newForm = false;
+
+    toggleNewForm() {
+        if (this.newForm) {
+            this.newForm = false;
+        } else {
+            this.newForm = true;
+        }
+    }
+}
