4040d3ceaae81e8651e04eb7834bd61e5a3c1606
axdevil <deb.devel@outlook.es>
2024-03-04 19:16:45 -0600

    Ruta para obtencion de pacientes

--- api/controller/patient.ts
+++ api/controller/patient.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from 'express';
 import { ResponseInternalError } from '../utils/functions';
+import { SortOrder } from 'mongoose';
 import Patient from '../model/patient';
 
 export default {
+    get: async function (req: Request, res: Response) {
+        let sort = req.query.sort;
+        let order: SortOrder = 1;
+        //INICIALIZACION DE PARAMETROS SORT
+        if (!sort) {
+            sort = 'createdAt';
+        }
+        if (req.query.order == 'desc') {
+            order = -1;
+        }
+
+        try {
+            //BUSQUEDA EN DB
+            const patients = await Patient.find({ user: req.user })
+                .sort([[`${sort}`, order]])
+                .select('name lastname phone birthday gender')
+                .lean();
+
+            res.status(200).json(patients);
+        } catch (error: any) {
+            ResponseInternalError(res, error);
+        }
+    },
     new: async function (req: Request, res: Response) {
         const { name, lastname, phone, birthday, gender } = req.body;
 
@@ -24,15 +48,15 @@ export default {
         const patient = await Patient.findById(id).lean();
 
         if (id) {
-            if(patient){
+            if (patient) {
                 try {
                     await Patient.findByIdAndDelete(id);
                     res.status(200).json({ msg: 'Patient deleted' });
                 } catch (error: any) {
                     ResponseInternalError(res, error);
                 }
-            }else{
-                res.status(400).json({msg: 'Patient not found in DB'})
+            } else {
+                res.status(400).json({ msg: 'Patient not found in DB' });
             }
         } else {
             ResponseInternalError(res, 'Id required');
--- api/routes/patient.ts
+++ api/routes/patient.ts
@@ -3,6 +3,7 @@ import { verifyToken } from '../utils/token';
 import controller from '../controller/patient';
 const router = Router();
 
+router.get('/get', verifyToken, controller.get)
 router.post('/new', verifyToken, controller.new)
 router.delete('/delete', verifyToken, controller.delete)
 
