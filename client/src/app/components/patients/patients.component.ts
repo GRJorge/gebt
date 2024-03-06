6ef263197d198e6408ba48ef401afd9609e1e93a
axdevil <deb.devel@outlook.es>
2024-03-06 11:18:16 -0600

    Guardado de pacientes

--- client/src/app/components/patients/patients.component.ts
+++ client/src/app/components/patients/patients.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewPatientComponent } from './new-patient/new-patient.component';
+import { TableComponent } from '../general/table/table.component';
+import { PatientService } from '../../services/patient.service';
+import { Patients } from '../../interfaces/patient.interface';
 
 @Component({
     selector: 'patients',
     standalone: true,
-    imports: [NewPatientComponent],
+    imports: [NewPatientComponent, TableComponent],
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss',
 })
-export class PatientsComponent {
+export class PatientsComponent implements OnInit {
+    patients!: Patients[];
     newForm = false;
 
+    constructor(private patientService: PatientService) {}
+
+    ngOnInit(): void {
+        this.patientService.get().subscribe((data: any) => {
+            this.patients = data;
+        });
+    }
+
     toggleNewForm() {
-        if (this.newForm) {
-            this.newForm = false;
-        } else {
-            this.newForm = true;
-        }
+        this.newForm = !this.newForm;
     }
 }
