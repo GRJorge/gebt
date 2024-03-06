c1a44a2c72d84215bd0e7e0422e03ddcc4411b50
axdevil <deb.devel@outlook.es>
2024-03-06 13:20:59 -0600

    Formato de fechas

--- client/src/app/components/general/table/table.component.ts
+++ client/src/app/components/general/table/table.component.ts
@@ -15,6 +15,14 @@ export class TableComponent {
         return Object.keys(obj);
     }
     getKeysValue(obj: any, key: string): string {
-        return obj[key];
+        const value = obj[key];
+        
+        console.log(value)
+        if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(value)) {
+            const date = new Date(value)
+            return `${date.getDate() + 1 < 10 ? '0' : ''}${date.getDate() + 1}/${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}/${date.getFullYear()}`;
+        } else {
+            return value;
+        }
     }
 }
