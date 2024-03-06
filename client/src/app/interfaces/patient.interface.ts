6fc019692218b3d816846f2509d76791d6267a71
axdevil <deb.devel@outlook.es>
2024-03-06 11:17:56 -0600

    Interface para pacientes

--- client/src/app/interfaces/patient.interface.ts
+++ client/src/app/interfaces/patient.interface.ts
@@ -5,3 +5,11 @@ export interface Patient {
     birthday: Date;
     gender: string;
 }
+export interface Patients {
+    _id: string;
+    name: string;
+    lastname: string;
+    phone: string;
+    birthday: Date;
+    gender: string;
+}
