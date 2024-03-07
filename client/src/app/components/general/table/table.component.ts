36cbbaa6654691396a50a74db1606234190a5240
axdevil <deb.devel@outlook.es>
2024-03-07 10:54:39 -0600

    Botones de eliminacion y edicion

--- client/src/app/components/general/table/table.component.ts
+++ client/src/app/components/general/table/table.component.ts
@@ -16,10 +16,9 @@ export class TableComponent {
     }
     getKeysValue(obj: any, key: string): string {
         const value = obj[key];
-        
-        console.log(value)
+
         if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(value)) {
-            const date = new Date(value)
+            const date = new Date(value);
             return `${date.getDate() + 1 < 10 ? '0' : ''}${date.getDate() + 1}/${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}/${date.getFullYear()}`;
         } else {
             return value;
