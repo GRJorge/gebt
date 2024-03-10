4306d93d08432088f39a66b37d2e2b018ad3c1de
axdevil <deb.devel@outlook.es>
2024-03-09 19:53:56 -0600

    Rediseño del top-panel

--- client/src/app/components/home/top-panel/top-panel.component.ts
+++ client/src/app/components/home/top-panel/top-panel.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from '../../general/logo/logo.component';
 
 @Component({
     selector: 'top-panel',
     standalone: true,
-    imports: [LogoComponent],
+    imports: [],
     templateUrl: './top-panel.component.html',
     styleUrl: './top-panel.component.scss',
 })
