b9503bf8c27355dc4509a1b00299ad2aa687d75e
axdevil <deb.devel@outlook.es>
2024-03-11 09:43:57 -0600

    Formulario para nuevo paciente

---
+++ client/src/app/components/patients/new-patient/new-patient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'new-patient',
+    standalone: true,
+    imports: [],
+    templateUrl: './new-patient.component.html',
+    styleUrl: './new-patient.component.scss',
+})
+export class NewPatientComponent {}
