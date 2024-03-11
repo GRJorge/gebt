482527c6bf0e903cf1acfec01f193af59877c3d9
axdevil <deb.devel@outlook.es>
2024-03-11 08:49:24 -0600

    Patients Component

---
+++ client/src/app/components/patients/patients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'patients',
+    standalone: true,
+    imports: [],
+    templateUrl: './patients.component.html',
+    styleUrl: './patients.component.scss',
+})
+export class PatientsComponent {}
