99372e99d84ac6419e2769d2a57e26268bd5519e
axdevil <deb.devel@outlook.es>
2024-03-12 12:55:25 -0600

    Relleno de datos de pacientes

--- client/src/app/components/patients/patient-card/patient-card.component.ts
+++ client/src/app/components/patients/patient-card/patient-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Patient } from '../../../interfaces/patient.interface';
 
 @Component({
     selector: 'patient-card',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
     templateUrl: './patient-card.component.html',
     styleUrl: './patient-card.component.scss',
 })
-export class PatientCardComponent {}
+export class PatientCardComponent {
+    @Input() patient!: Patient;
+
+    calculateAge(): string {
+        const birthday = new Date(this.patient.birthday);
+        const date = new Date();
+
+        let age = date.getFullYear() - birthday.getFullYear();
+        const month = date.getMonth() - birthday.getMonth();
+        let text = 'años';
+
+        if (month < 0 || (month === 0 && date.getDate() < birthday.getDate() + 1)) {
+            age--;
+        }
+        if (age === 1) {
+            text = 'año';
+        }
+
+        return `${age} ${text}`;
+    }
+}
--- client/src/app/components/patients/patients.component.ts
+++ client/src/app/components/patients/patients.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewPatientComponent } from './new-patient/new-patient.component';
 import { PatientCardComponent } from './patient-card/patient-card.component';
 import { TableComponent } from '../general/table/table.component';
+import { PatientService } from '../../services/patient.service';
+import { Patients } from '../../interfaces/patient.interface';
 
 @Component({
     selector: 'patients',
@@ -10,8 +12,18 @@ import { TableComponent } from '../general/table/table.component';
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss',
 })
-export class PatientsComponent {
+export class PatientsComponent implements OnInit {
+    constructor(private patientService: PatientService) {}
+    patients?: Patients[];
     viewNewForm = false;
+
+    ngOnInit(): void {
+        this.patientService.get().subscribe({
+            next: (data: Patients | any) => {
+                this.patients = data;
+            },
+        });
+    }
 
     toggleNewForm() {
         this.viewNewForm = !this.viewNewForm;
