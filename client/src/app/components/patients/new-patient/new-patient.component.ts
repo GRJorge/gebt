b37360140fac85afb141bc9e367fe55f14196f43
axdevil <deb.devel@outlook.es>
2024-03-11 19:07:02 -0600

    Validacion de formulario

--- client/src/app/components/patients/new-patient/new-patient.component.ts
+++ client/src/app/components/patients/new-patient/new-patient.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationComponent } from '../../general/notification/notification.component';
 
 @Component({
     selector: 'new-patient',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, NotificationComponent],
     templateUrl: './new-patient.component.html',
     styleUrl: './new-patient.component.scss',
 })
-export class NewPatientComponent {}
+export class NewPatientComponent {
+    form = new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        lastname: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        phone: new FormControl('', [Validators.minLength(10), Validators.maxLength(10)]),
+        birthday: new FormControl('', Validators.required),
+        gender: new FormControl('F', Validators.required),
+    });
+
+    getControlError(control: string, error: string): boolean {
+        return this.form.get(control)!.getError(error);
+    }
+}
