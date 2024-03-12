a7e94288ae0ae74fd411b36fdfa798716e07caf6
axdevil <deb.devel@outlook.es>
2024-03-12 11:44:09 -0600

    Patient card

---
+++ client/src/app/components/patients/patient-card/patient-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'patient-card',
+    standalone: true,
+    imports: [],
+    templateUrl: './patient-card.component.html',
+    styleUrl: './patient-card.component.scss',
+})
+export class PatientCardComponent {}
