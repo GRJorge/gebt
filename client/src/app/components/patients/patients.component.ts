1ac393977bfd0d3de5c3868b7c733a7fa6732b8d
axdevil <deb.devel@outlook.es>
2024-03-12 11:44:41 -0600

    Layout con patient card

--- client/src/app/components/patients/patients.component.ts
+++ client/src/app/components/patients/patients.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NewPatientComponent } from './new-patient/new-patient.component';
+import { PatientCardComponent } from './patient-card/patient-card.component';
+import { TableComponent } from '../general/table/table.component';
 
 @Component({
     selector: 'patients',
     standalone: true,
-    imports: [],
+    imports: [NewPatientComponent, PatientCardComponent, TableComponent],
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss',
 })
-export class PatientsComponent {}
+export class PatientsComponent {
+    viewNewForm = false;
+
+    toggleNewForm() {
+        this.viewNewForm = !this.viewNewForm;
+    }
+}
