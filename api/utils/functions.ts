b4c7479fe97a3e686a6ae1a0c01b13ed7fed434a
axdevil <deb.devel@outlook.es>
2024-03-14 12:27:58 -0600

    Imprimir error en consola

--- api/utils/functions.ts
+++ api/utils/functions.ts
@@ -2,4 +2,5 @@ import { Response } from 'express';
 
 export function ResponseInternalError(res: Response, error: any) {
     res.status(500).json({ msg: 'Internal Error', error });
+    console.log(error);
 }
