0449b8b41098de76fc07c1002a5d70f2201532f5
axdevil <deb.devel@outlook.es>
2024-03-15 01:30:27 -0600

    Guardado de tema en cookie

--- client/src/app/services/cookies.service.ts
+++ client/src/app/services/cookies.service.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CookiesService {
-    constructor(private cookieService: CookieService) {}
+    expirationDate = new Date();
+
+    constructor(private cookieService: CookieService) {
+        this.expirationDate.setDate(this.expirationDate.getDate() + 30);
+    }
 
     createSessionCookie(token: string) {
-        let expirationDate = new Date();
-        expirationDate.setDate(expirationDate.getDate() + 30);
+        this.cookieService.set('session', token, { expires: this.expirationDate, sameSite: 'Strict' });
+    }
 
-        this.cookieService.set('session', token, { expires: expirationDate, sameSite: 'Strict' });
+    themeCookie(theme: string) {
+        this.cookieService.set('theme', theme, { expires: this.expirationDate });
+    }
+    getThemeCookie(): string {
+        return this.cookieService.get('theme');
     }
 }
--- client/src/app/services/theme.service.ts
+++ client/src/app/services/theme.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, signal } from '@angular/core';
+import { CookiesService } from './cookies.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ThemeService {
-    theme = signal<string>('lightTheme');
+    constructor(private cookiesService: CookiesService) {}
+
+    theme = signal<string>(this.cookiesService.getThemeCookie() === '' ? 'lightTheme' : this.cookiesService.getThemeCookie());
 
     updateTheme() {
         this.theme.update((value) => (value === 'darkTheme' ? 'lightTheme' : 'darkTheme'));
+        this.cookiesService.themeCookie(this.theme());
+        console.log(this.cookiesService.getThemeCookie())
     }
 }
