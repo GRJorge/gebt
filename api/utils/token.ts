c1f5b9c809be3cebd49e67dc03f812b46ac544e9
grjorge <deb.devel@outlook.es>
2024-03-19 19:15:11 -0600

    Comprobacion de existencia de usuario al verificar el token

--- api/utils/token.ts
+++ api/utils/token.ts
@@ -3,6 +3,7 @@ import { Response, Request, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { jwtDecode } from 'jwt-decode';
 import { ResponseInternalError } from './functions';
+import User from '../model/user';
 
 export function createUserToken(res: Response, user: string, fun: Function) {
     jwt.sign({ user }, process.env.SECRET_KEY!, { expiresIn: '30 days' }, (error, token) => {
@@ -27,13 +28,20 @@ export function verifyToken(req: Request, res: Response, next: NextFunction) {
         //REVISAR SI EXISTE TOKEN
         const token = bearerHeader.split(' ')[1];
         //VERIFICAR EL TOKEN
-        jwt.verify(token, process.env.SECRET_KEY!, (error, decoded) => {
+        jwt.verify(token, process.env.SECRET_KEY!, async (error, decoded) => {
             if (error) {
                 res.status(403).json({ msg: 'Invalid Token' });
             } else {
                 //GUARDADO DE ID DE ACTUAL TOKEN EN EL REQ
                 req.user = jwtDecode<decodeToken>(token).user;
-                next();
+                //COMPROBAR QUE EL USUARIO EXISTE EN LA DB
+                const user = await User.findById(req.user).lean();
+
+                if (user) {
+                    next();
+                } else {
+                    res.status(403).json({ msg: 'User not exist in DB' });
+                }
             }
         });
     } else {
