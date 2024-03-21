632d19d9ae296dfdeace7cd1a093d68cd8df1008
grjorge <deb.devel@outlook.es>
2024-03-20 20:00:41 -0600

    Importacion de Dates

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component';
 import { TopPanelComponent } from './top-panel/top-panel.component';
 import { PatientsComponent } from '../patients/patients.component';
-import { UserService } from '../../services/user.service';
+import { DatesComponent } from '../dates/dates.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [NavigationComponent, TopPanelComponent, PatientsComponent],
+    imports: [NavigationComponent, TopPanelComponent, PatientsComponent, DatesComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-    numSection = 0;
+    numSection = 2;
     titleTopPanel = ['Dashboard', 'Pacientes', 'Citas', 'Dietas'];
 
     //CAMBIO DE NUMSECTION
