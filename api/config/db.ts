b579edae868c60bf4646de0f90a0713ffad25b40
grjorge <deb.devel@outlook.es>
2024-03-22 23:35:32 -0600

    Conexion a base de datos asincrona

--- api/config/db.ts
+++ api/config/db.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-function connect() {
+async function connect() {
     try {
-        mongoose.connect(process.env.DB_URL + process.env.DB_NAME!);
+        await mongoose.connect(process.env.DB_URL + process.env.DB_NAME!);
         console.log('DB Connected to: ' + process.env.DB_NAME);
     } catch (Error) {
         console.error('DB Error: ' + Error);
