7ab4427e21f8a1a7b635c309d420c1ec7c64c6fa
grjorge <deb.devel@outlook.es>
2024-03-25 00:23:58 -0600

    Funciones para calcular IMC, GEB y GET

--- api/utils/functions.ts
+++ api/utils/functions.ts
@@ -4,3 +4,39 @@ export function ResponseInternalError(res: Response, error: any) {
     res.status(500).json({ msg: 'Internal Error', error });
     console.log(error);
 }
+
+export function calculateIMC(weight: number, height: number): number {
+    return weight / Math.pow(height / 100, 2);
+}
+
+export function calculateGEB(weight: number, height: number, imc: number, gender: string, age: number): number {
+    if (imc < 25) {
+        if (gender === 'F') {
+            return 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+        } else {
+            return 66.47 + 13.75 * weight + 5 * height - 6.78 * age;
+        }
+    } else {
+        let temp = 10 * weight + 6.25 * height - 5 * age;
+
+        if (gender === 'F') {
+            temp -= 161;
+        } else {
+            temp += 5;
+        }
+
+        return temp;
+    }
+}
+
+export function calculateGET(geb: number, af: number, imc: number): number {
+    let temp = geb + (geb / 100) * af;
+
+    if (imc < 18.5) {
+        return temp + 100;
+    } else if (imc < 25) {
+        return temp;
+    } else {
+        return temp + (geb / 100) * 10;
+    }
+}
