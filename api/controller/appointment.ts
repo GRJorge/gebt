d3e8b40c8afc1b4ac25c529047ac2e15a6fee73e
grjorge <deb.devel@outlook.es>
2024-03-25 19:53:45 -0600

    Guardado de citas en DB

--- api/controller/appointment.ts
+++ api/controller/appointment.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from 'express';
+import Patient from '../model/patient';
+import Appointment from '../model/appointment';
+import { ResponseInternalError, calculateGEB, calculateGET, calculateIMC, getAge } from '../utils/functions';
 
 export default {
-    new: function (req: Request, res: Response) {
-        const { date, hour, state, height, weight } = req.body; //imc, af, geb, get
+    new: async function (req: Request, res: Response) {
+        const { date, datetime, state, height, weight, af, patient } = req.body; //imc, geb, get
+
+        try {
+            const patientInfo = await Patient.findById(patient).select('gender birthday').lean();
+
+            if (patientInfo) {
+                const imc = calculateIMC(weight, height);
+                const geb = calculateGEB(weight, height, imc, patientInfo.gender, getAge(patientInfo.birthday));
+                const get = calculateGET(geb, af, imc);
+
+                await new Appointment({
+                    date,
+                    datetime,
+                    state,
+                    height,
+                    weight,
+                    imc,
+                    af,
+                    geb,
+                    get,
+                    patient,
+                    user: req.user,
+                }).save();
+            } else {
+                res.status(400).json({ msg: 'Patient not found' });
+            }
+        } catch (error) {
+            ResponseInternalError(res, error);
+        }
     },
 };
