edede694ca05a4c49816fdb3c5cd6ee3c411d823
grjorge <deb.devel@outlook.es>
2024-03-26 20:21:04 -0600

    Formulario para creacion de citas

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewAppointmentComponent } from './new-appointment/new-appointment.component';
 
 @Component({
     selector: 'appointments',
     standalone: true,
-    imports: [],
+    imports: [NewAppointmentComponent],
     templateUrl: './appointments.component.html',
     styleUrl: './appointments.component.scss',
 })
---
+++ client/src/app/components/appointments/new-appointment/new-appointment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'new-appointment',
+    standalone: true,
+    imports: [],
+    templateUrl: './new-appointment.component.html',
+    styleUrl: './new-appointment.component.scss',
+})
+export class NewAppointmentComponent {}
