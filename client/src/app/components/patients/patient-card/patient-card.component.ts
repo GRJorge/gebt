43f1147e3551a3ff50b716b387178dbe98acf955
grjorge <deb.devel@outlook.es>
2024-03-27 20:00:28 -0600

    Calculo de edades nuevo en pacientes

--- client/src/app/components/patients/patient-card/patient-card.component.ts
+++ client/src/app/components/patients/patient-card/patient-card.component.ts
@@ -10,31 +10,13 @@ import { PatientService } from '../../../services/patient.service';
     styleUrl: './patient-card.component.scss',
 })
 export class PatientCardComponent {
-    constructor(private patientService: PatientService) {}
+    constructor(public patientService: PatientService) {}
 
     @Input() patient!: Patient;
 
     @Output() update = new EventEmitter();
     @Output() edit = new EventEmitter<Patient>();
     sureDelete = false;
-
-    calculateAge(): string {
-        const birthday = new Date(this.patient.birthday);
-        const date = new Date();
-
-        let age = date.getFullYear() - birthday.getFullYear();
-        const month = date.getMonth() - birthday.getMonth();
-        let text = 'años';
-
-        if (month < 0 || (month === 0 && date.getDate() < birthday.getDate() + 1)) {
-            age--;
-        }
-        if (age === 1) {
-            text = 'año';
-        }
-
-        return `${age} ${text}`;
-    }
 
     editEvent() {
         this.edit.emit(this.patient);
