91eda81769bb30daee9b8f3ed0ddcedf00ce252c
grjorge <deb.devel@outlook.es>
2024-03-27 19:38:43 -0600

    Formulario reactivo

--- client/src/app/components/appointments/new-appointment/new-appointment.component.ts
+++ client/src/app/components/appointments/new-appointment/new-appointment.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../../../services/patient.service';
 import { Patients } from '../../../interfaces/patient.interface';
 import { PatientAppointmentComponent } from './patient-appointment/patient-appointment.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationComponent } from '../../general/notification/notification.component';
 
 @Component({
     selector: 'new-appointment',
     standalone: true,
-    imports: [PatientAppointmentComponent],
+    imports: [PatientAppointmentComponent, NotificationComponent, ReactiveFormsModule],
     templateUrl: './new-appointment.component.html',
     styleUrl: './new-appointment.component.scss',
 })
@@ -21,5 +23,20 @@ export class NewAppointmentComponent implements OnInit {
                 this.patients = result;
             },
         });
+    }
+
+    appointmentForm = new FormGroup({
+        date: new FormControl('', Validators.required),
+        datetime: new FormControl('', Validators.required),
+    });
+
+    idPatient?: string;
+    namePatient: string = 'Selecciona un paciente';
+    lastnamePatient: string = '';
+
+    setPatient(data: string[]) {
+        this.idPatient = data[0];
+        this.namePatient = data[1];
+        this.lastnamePatient = data[2];
     }
 }
