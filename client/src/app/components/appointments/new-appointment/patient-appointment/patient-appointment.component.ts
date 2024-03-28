85bc6d14ae7f89bc7fd664e41fd989466353901e
grjorge <deb.devel@outlook.es>
2024-03-27 20:00:08 -0600

    Calculo de edades en citas

--- client/src/app/components/appointments/new-appointment/patient-appointment/patient-appointment.component.ts
+++ client/src/app/components/appointments/new-appointment/patient-appointment/patient-appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PatientService } from '../../../../services/patient.service';
 
 @Component({
     selector: 'patient-appointment',
@@ -8,9 +9,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     styleUrl: './patient-appointment.component.scss',
 })
 export class PatientAppointmentComponent {
+    constructor(public patientService: PatientService) {}
+
     @Input() name!: string;
     @Input() lastname!: string;
     @Input() gender!: string;
+    @Input() birthday!: Date;
     @Input() id!: string;
 
     @Output() patientClickedEvent = new EventEmitter<string[]>();
