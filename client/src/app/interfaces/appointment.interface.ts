9d2705eb7585af720cb1fe29789fc5a3abc36883
grjorge <deb.devel@outlook.es>
2024-04-03 20:01:33 -0600

    Cambio de fecha en interfaz

--- client/src/app/interfaces/appointment.interface.ts
+++ client/src/app/interfaces/appointment.interface.ts
@@ -1,4 +1,10 @@
 export interface NewAppointment {
-    datetime: Date;
+    date: {
+        day: { type: Number, required: true },
+        month: { type: Number, required: true },
+        year: { type: Number, required: true },
+        hour: { type: Number, required: true },
+        minute: { type: Number, required: true },
+    },
     patient: string;
 }
