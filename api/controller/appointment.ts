e58e8158caa6ca07be7667e0b24a635010a1ca77
grjorge <deb.devel@outlook.es>
2024-04-04 23:42:09 -0600

    Guardado de cita

--- api/controller/appointment.ts
+++ api/controller/appointment.ts
@@ -5,13 +5,13 @@ import { ResponseInternalError, calculateGEB, calculateGET, calculateIMC, getAge
 
 export default {
     new: async function (req: Request, res: Response) {
-        const { datetime, patient } = req.body;
+        const { date, patient } = req.body;
 
         try {
             const patientInfo = await Patient.findById(patient).select('gender birthday').lean();
 
             if (patientInfo) {
-                await new Appointment({ datetime, patient, user: req.user }).save();
+                await new Appointment({ date, patient, user: req.user }).save();
                 res.status(200).json({ msg: 'Appointment created' });
             } else {
                 res.status(400).json({ msg: 'Patient not found' });
@@ -19,5 +19,8 @@ export default {
         } catch (error) {
             ResponseInternalError(res, error);
         }
+    },
+    overlapAppointment: async function (req: Request, res: Response) {
+        
     },
 };
