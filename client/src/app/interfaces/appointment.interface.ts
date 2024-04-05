89ec38a896c4234a9e60a3ad0d6e4532d39bb7ca
grjorge <deb.devel@outlook.es>
2024-04-05 01:29:35 -0600

    Tipado de citas aproximadas y citas

--- client/src/app/interfaces/appointment.interface.ts
+++ client/src/app/interfaces/appointment.interface.ts
@@ -16,3 +16,23 @@ export interface DateObject {
     minute: number;
     time: string;
 }
+export interface ApproximateAppointment {
+    _id: string;
+    date: DateObject;
+    state: number;
+    patient: {
+        _id: string;
+        name: string;
+        lastname: string;
+    };
+    user: string;
+    createdAt: Date;
+}
+export interface Appointment {
+    _id: string;
+    date: DateObject;
+    state: number;
+    patient: string;
+    user: string;
+    createdAt: Date;
+}
