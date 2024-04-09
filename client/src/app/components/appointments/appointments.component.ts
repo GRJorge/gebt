9a8896cfb3d5f2b8788dff5f3964e3a3e1db83c5
grjorge <deb.devel@outlook.es>
2024-04-09 10:51:08 -0600

    Layout de citas

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NewAppointmentComponent } from './new-appointment/new-appointment.component';
+import { AppointmentCardComponent } from './appointment-card/appointment-card.component';
 
 @Component({
     selector: 'appointments',
     standalone: true,
-    imports: [NewAppointmentComponent],
+    imports: [NewAppointmentComponent, AppointmentCardComponent],
     templateUrl: './appointments.component.html',
     styleUrl: './appointments.component.scss',
 })
