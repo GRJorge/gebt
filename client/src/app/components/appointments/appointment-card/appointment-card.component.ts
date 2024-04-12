456149a89b5359138d265f0fbb5d3587a302a944
grjorge <deb.devel@outlook.es>
2024-04-12 01:11:32 -0600

    Relleno de datos de citas

--- client/src/app/components/appointments/appointment-card/appointment-card.component.ts
+++ client/src/app/components/appointments/appointment-card/appointment-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Appointment } from '../../../interfaces/appointment.interface';
 
 @Component({
     selector: 'appointment-card',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
     templateUrl: './appointment-card.component.html',
     styleUrl: './appointment-card.component.scss',
 })
-export class AppointmentCardComponent {}
+export class AppointmentCardComponent {
+    @Input() appointment!: Appointment;
+
+    //ESTADO DE LA CITA
+    //0: Pendiente, 1: Activo, 2: Cancelado, 3: No asistido
+    stateString(): string {
+        switch (this.appointment.state) {
+            case 0:
+                return 'Pendiente';
+            case 1:
+                return 'Activo';
+            case 2:
+                return 'Cancelado';
+            default:
+                return 'No asistido';
+        }
+    }
+    //DIA DE LA CITA
+    dayWeekString(): string {
+        const date = new Date();
+        const dateAppointment = new Date(`${this.appointment.date.year}-${this.appointment.date.month + 1}-${this.appointment.date.day}`);
+
+        switch (dateAppointment.getDate() - date.getDate()) {
+            case 0:
+                return 'Hoy';
+            case 1:
+                return 'Mañana';
+            default:
+                return dayToString(dateAppointment.getDay());
+        }
+
+        function dayToString(day: number) {
+            const days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+            return days[day];
+        }
+    }
+}
