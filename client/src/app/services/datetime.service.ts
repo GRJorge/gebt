28ee208cc75b67190b2c9e1cf8b286e29d315062
grjorge <deb.devel@outlook.es>
2024-04-15 19:08:24 -0600

    Condicial correspondiente a cuando la hora es 12pm

--- client/src/app/services/datetime.service.ts
+++ client/src/app/services/datetime.service.ts
@@ -17,7 +17,7 @@ export class DatetimeService {
         if (hour < 12) {
             return `${hour === 0 ? 12 : hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')} ${ampm}`;
         } else {
-            return `${(hour - 12).toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')} ${ampm}`;
+            return `${(hour === 12 ? hour : hour - 12).toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')} ${ampm}`;
         }
     }
 }
