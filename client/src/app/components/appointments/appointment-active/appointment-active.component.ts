7d89c63f1ee51452c9fe9de6a479faceb25140f4
grjorge <deb.devel@outlook.es>
2024-04-24 12:02:37 -0600

    Validacion de formulario

--- client/src/app/components/appointments/appointment-active/appointment-active.component.ts
+++ client/src/app/components/appointments/appointment-active/appointment-active.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationComponent } from '../../general/notification/notification.component';
 
 @Component({
     selector: 'appointment-active',
     standalone: true,
-    imports: [],
+    imports: [NotificationComponent, ReactiveFormsModule],
     templateUrl: './appointment-active.component.html',
     styleUrl: './appointment-active.component.scss',
 })
-export class AppointmentActiveComponent {}
+export class AppointmentActiveComponent {
+    form = new FormGroup({
+        weight: new FormControl('', [Validators.required, Validators.min(1), Validators.max(300)]),
+        height: new FormControl('', [Validators.required, Validators.min(1), Validators.max(300)]),
+        af: new FormControl('', [Validators.required, Validators.min(0), Validators.max(100)]),
+    });
+
+    getFormError(form: string, error: string): boolean {
+        return this.form.get(form)!.getError(error);
+    }
+}
