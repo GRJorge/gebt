ecc7364f01bacbeb076ee721d0abeb6674dad570
grjorge <deb.devel@outlook.es>
2024-04-23 23:30:23 -0600

    Formulario para citas activas

---
+++ client/src/app/components/appointments/appointment-active/appointment-active.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'appointment-active',
+    standalone: true,
+    imports: [],
+    templateUrl: './appointment-active.component.html',
+    styleUrl: './appointment-active.component.scss',
+})
+export class AppointmentActiveComponent {}
