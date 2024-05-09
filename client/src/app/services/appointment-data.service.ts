fb45dd13e3bcfcd75073eff6647241ac4ade9ebf
grjorge <deb.devel@outlook.es>
2024-05-09 01:30:31 -0600

    Peticion a obtencion de datos por paciente

--- client/src/app/services/appointment-data.service.ts
+++ client/src/app/services/appointment-data.service.ts
@@ -15,6 +15,9 @@ export class AppointmentDataService {
         return this.http.post(this.apiUrl + 'set', data);
     }
     get(appointment: string) {
-        return this.http.get(this.apiUrl + 'get',{params: {appointment}})
+        return this.http.get(this.apiUrl + 'get', { params: { appointment } });
+    }
+    getByPatient(patient: string) {
+        return this.http.get(this.apiUrl + 'getByPatient', { params: { patient } });
     }
 }
