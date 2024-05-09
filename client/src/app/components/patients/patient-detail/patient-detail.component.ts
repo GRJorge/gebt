26648174d8bf6da1bdd449de4ed47221e588c7f8
grjorge <deb.devel@outlook.es>
2024-05-09 01:31:31 -0600

    Datos reales en graficas y carga de contenido cuando todos los datos estan listos

--- client/src/app/components/patients/patient-detail/patient-detail.component.ts
+++ client/src/app/components/patients/patient-detail/patient-detail.component.ts
@@ -6,20 +6,27 @@ import { Appointment } from '../../../interfaces/appointment.interface';
 import { AppointmentCardComponent } from '../../appointments/appointment-card/appointment-card.component';
 import { ChartConfiguration, ChartEvent, ChartType, ChartOptions } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { AppointmentDataService } from '../../../services/appointment-data.service';
+import { AppointmentData } from '../../../interfaces/appointmentData.interface';
+import { LoadingComponent } from '../../general/loading/loading.component';
+import { DatetimeService } from '../../../services/datetime.service';
 
 @Component({
     selector: 'patient-detail',
     standalone: true,
-    imports: [AppointmentCardComponent, BaseChartDirective],
+    imports: [AppointmentCardComponent, BaseChartDirective, LoadingComponent],
     templateUrl: './patient-detail.component.html',
     styleUrl: './patient-detail.component.scss',
 })
 export class PatientDetailComponent implements OnInit {
-    constructor(public patientService: PatientService, private appointmentService: AppointmentService) {}
+    constructor(public patientService: PatientService, private appointmentService: AppointmentService, private appointmentDataService: AppointmentDataService, private datetimeService: DatetimeService) {}
 
     @Input() id!: string;
     patient!: Patient;
     appointments!: Appointment[];
+    weightChartData: number[] = [];
+    imcChartData: number[] = [];
+    labelsChart: string[] = [];
 
     ngOnInit(): void {
         this.patientService.getById(this.id).subscribe({
@@ -33,20 +40,40 @@ export class PatientDetailComponent implements OnInit {
                 this.appointments = data;
             },
         });
+
+        this.appointmentDataService.getByPatient(this.id).subscribe({
+            next: (data: AppointmentData[] | any) => {
+                for (const appointmentData of data) {
+                    this.weightChartData.push(appointmentData.weight);
+                    this.imcChartData.push(appointmentData.imc);
+                    this.labelsChart.push(this.datetimeService.dateToString(new Date(appointmentData.createdAt)));
+                }
+            },
+        });
     }
 
-    lineChartData: ChartConfiguration<'line'>['data'] = {
-        labels: ['23/04/2024', '30/04/2024', '10/05/2024'],
+    weightLineChartData: ChartConfiguration<'line'>['data'] = {
+        labels: this.labelsChart,
         datasets: [
             {
-                data: [2, 4, 5],
+                data: this.weightChartData,
                 label: 'Peso',
-                tension: 0.1,
             },
         ],
     };
+
+    imcLineChartData: ChartConfiguration<'line'>['data'] = {
+        labels: this.labelsChart,
+        datasets: [
+            {
+                data: this.imcChartData,
+                label: 'Peso',
+            },
+        ],
+    };
+
     lineChartOptions: ChartOptions<'line'> = {
         responsive: true,
         borderColor: '#CC9CD9',
-    }
+    };
 }
