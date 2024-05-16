e3833148b354af6d766bf8387ff5356481be13ea
grjorge <deb.devel@outlook.es>
2024-05-15 19:56:32 -0600

    Layout principal para settings

--- client/src/app/components/settings/settings.component.ts
+++ client/src/app/components/settings/settings.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../interfaces/user.interface';
+import { NotificationComponent } from '../general/notification/notification.component';
 
 @Component({
     selector: 'settings',
     standalone: true,
-    imports: [],
+    imports: [NotificationComponent],
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit {
+    constructor(private userService: UserService) {}
+
+    user!: User;
+
+    ngOnInit(): void {
+        this.userService.get().subscribe({
+            next: (data: User | any) => {
+                this.user = data;
+            },
+        });
+    }
+}
