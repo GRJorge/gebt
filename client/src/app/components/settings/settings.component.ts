4123b1964cda77fb744610cb202ab491071c10ee
grjorge <deb.devel@outlook.es>
2024-05-16 00:10:38 -0600

    Validacion de formularios

--- client/src/app/components/settings/settings.component.ts
+++ client/src/app/components/settings/settings.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../interfaces/user.interface';
 import { NotificationComponent } from '../general/notification/notification.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'settings',
     standalone: true,
-    imports: [NotificationComponent],
+    imports: [NotificationComponent, ReactiveFormsModule],
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.scss',
 })
@@ -14,12 +15,30 @@ export class SettingsComponent implements OnInit {
     constructor(private userService: UserService) {}
 
     user!: User;
+    nameForm!: FormGroup;
+    passwordForm = new FormGroup({
+        actualPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        newPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        confirm: new FormControl('', Validators.required),
+    });
 
     ngOnInit(): void {
         this.userService.get().subscribe({
             next: (data: User | any) => {
                 this.user = data;
+
+                this.nameForm = new FormGroup({
+                    name: new FormControl(this.user?.name, [Validators.required, Validators.minLength(3)]),
+                    lastname: new FormControl(this.user?.lastname, [Validators.required, Validators.minLength(3)]),
+                });
             },
         });
+    }
+
+    getFormControlError(form: FormGroup, control: string, error: string): boolean {
+        return form.get(control)!.getError(error);
+    }
+    confirmPassword(): boolean {
+        return this.passwordForm.get('newPassword')!.value === this.passwordForm.get('confirm')!.value;
     }
 }
